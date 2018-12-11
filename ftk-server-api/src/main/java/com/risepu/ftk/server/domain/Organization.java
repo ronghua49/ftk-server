6475744cc2b6b69c874dfcd070a6b75648f40611
q-wang <apeidou@gmail.com>
2018-12-11 11:21:45 +0800

    遗漏@Entity

--- ftk-server-api/src/main/java/com/risepu/ftk/server/domain/Organization.java
+++ ftk-server-api/src/main/java/com/risepu/ftk/server/domain/Organization.java
@@ -1,9 +1,10 @@
 /**
- * 
+ *
  */
 package com.risepu.ftk.server.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,17 +13,20 @@ import net.lc4ever.framework.domain.AuditableObject;
 /**
  * @author q-wang
  */
+@Entity
 @Table(name = "FTK_ORGANIZATION")
 public class Organization extends AuditableObject<String> {
 
 	private String id;
 
+	@Override
 	@Id
 	@Column(name = "ID", length = 10)
 	public String getId() {
 		return id;
 	}
 
+	@Override
 	public void setId(String id) {
 		this.id = id;
 	}
--- ftk-server-api/src/main/java/com/risepu/ftk/server/domain/OrganizationUser.java
+++ ftk-server-api/src/main/java/com/risepu/ftk/server/domain/OrganizationUser.java
@@ -1,9 +1,10 @@
 /**
- * 
+ *
  */
 package com.risepu.ftk.server.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,17 +13,20 @@ import net.lc4ever.framework.domain.TimestampObject;
 /**
  * @author q-wang
  */
-@Table(name="FTK_ORGANIZATION_USER")
+@Entity
+@Table(name = "FTK_ORGANIZATION_USER")
 public class OrganizationUser extends TimestampObject<String> {
 
 	private String id;
 
+	@Override
 	@Id
-	@Column(name="ID", length=31)
+	@Column(name = "ID", length = 31)
 	public String getId() {
 		return id;
 	}
 
+	@Override
 	public void setId(String id) {
 		this.id = id;
 	}
