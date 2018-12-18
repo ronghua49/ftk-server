94533dc3f7463cc17f7bdc5791b89b54e5996bb8
q-wang <apeidou@gmail.com>
2018-12-18 14:45:52 +0800

    依赖缺失修改

--- ftk-web-b/src/main/java/com/risepu/ftk/web/b/controller/SampleController.java
+++ ftk-web-b/src/main/java/com/risepu/ftk/web/b/controller/SampleController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.risepu.ftk.web.b.controller;
 
@@ -8,13 +8,11 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.risepu.ftk.server.service.SampleService;
 import com.risepu.ftk.web.api.Response;
 import com.risepu.ftk.web.b.dto.RegistRequest;
 import com.risepu.ftk.web.b.dto.RegistResult;
@@ -23,21 +21,18 @@ import com.risepu.ftk.web.b.dto.RegistResult;
  * @author q-wang
  */
 @Controller
-@RequestMapping(path="/sample")
+@RequestMapping(path = "/sample")
 public class SampleController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	
-	@Autowired
-	private SampleService sampleService;
-	
-	@RequestMapping(path="/sample1")
+
+	@RequestMapping(path = "/sample1")
 	@ResponseBody
 	public Response<RegistResult> regist(@RequestBody RegistRequest request) throws Exception {
-		
+
 		logger.debug("Request Uri: /sample/sample1");
-		Response<List<String>> sample1 = Response.succeed(Arrays.asList(new String[] {"hello", "world"}));
-		
+		Response<List<String>> sample1 = Response.succeed(Arrays.asList(new String[] { "hello", "world" }));
+
 		RegistResult result = new RegistResult();
 		Response.failed(400, "错误请求");
 		return Response.succeed(result);
