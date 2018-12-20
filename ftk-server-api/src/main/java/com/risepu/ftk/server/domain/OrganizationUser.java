aed95c24872e7ee1af8ac469a73eec0688d3835e
ronghua49 <1540307734@qq.com>
2018-12-20 14:35:37 +0800

    修改企业类结构

--- ftk-server-api/src/main/java/com/risepu/ftk/server/domain/OrganizationUser.java
+++ ftk-server-api/src/main/java/com/risepu/ftk/server/domain/OrganizationUser.java
@@ -1,8 +1,13 @@
 package com.risepu.ftk.server.domain;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import net.lc4ever.framework.domain.TimestampObject;
@@ -15,64 +20,6 @@ import net.lc4ever.framework.domain.TimestampObject;
 @Entity
 @Table(name = "FTK_ORGANIZATION_USER")
 public class OrganizationUser extends TimestampObject<String> {
-
-	private static final long serialVersionUID = 1L;
-
-	/* 企业名称 */
-	private String organization;
-
-	@Column(name = "ORGANIZATION", length = 10)
-	public String getOrganization() {
-		return organization;
-	}
-
-	public void setOrganization(String organization) {
-		this.organization = organization;
-	}
-
-	private String address;
-
-	@Column(name = "ADDRESS", length = 31)
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	private String legalPerson;
-
-	@Column(name = "LEGAL_PERSON", length = 10)
-	public String getLegalPerson() {
-		return legalPerson;
-	}
-
-	public void setLegalPerson(String legalPerson) {
-		this.legalPerson = legalPerson;
-	}
-
-	private String phoneNo;
-
-	@Column(name = "PHONE_NO", length = 11)
-	public String getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-
-	private String imgPath;
-
-	@Column(name = "IMG_PATH", length = 225)
-	public String getImgPath() {
-		return imgPath;
-	}
-
-	public void setImgPath(String imgPath) {
-		this.imgPath = imgPath;
-	}
 
 	/** 手机号 */
 	private String id;
@@ -88,10 +35,11 @@ public class OrganizationUser extends TimestampObject<String> {
 	public void setId(String id) {
 		this.id = id;
 	}
-
+	
+	
 	private String password;
-
-	@Column(name = "PASSWORD", length = 32)
+	
+	@Column(name="PASSWORD",length=32)
 	public String getPassword() {
 		return password;
 	}
@@ -99,5 +47,23 @@ public class OrganizationUser extends TimestampObject<String> {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
+	
+	@ManyToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "ORGANIZATION_ID")
+	private Organization organization;
+
+	public Organization getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(Organization organization) {
+		this.organization = organization;
+	}
+	
+	
+	
+	
 
 }
