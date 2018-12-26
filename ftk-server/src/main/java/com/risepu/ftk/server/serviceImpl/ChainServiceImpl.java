c97f1d9ea473f06e0b4307e28a59e0d21170e9df
q-wang <apeidou@gmail.com>
2018-12-26 15:49:38 +0800

    链模拟接口

---
+++ ftk-server/src/main/java/com/risepu/ftk/server/serviceImpl/ChainServiceImpl.java
@@ -0,0 +1,50 @@
+/**
+ *
+ */
+package com.risepu.ftk.server.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.token.Sha512DigestUtils;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.risepu.ftk.server.domain.DocumentData;
+import com.risepu.ftk.server.domain.ProofDocument;
+import com.risepu.ftk.server.service.ChainService;
+
+import net.lc4ever.framework.service.GenericCrudService;
+
+/**
+ * @author q-wang
+ */
+@Service
+public class ChainServiceImpl implements ChainService {
+
+	private GenericCrudService crudService;
+
+	@Autowired
+	public void setCrudService(GenericCrudService crudService) {
+		this.crudService = crudService;
+	}
+
+	@Override
+	public String sign(Long documentId) {
+		//		ProofDocument document = crudService.get(ProofDocument.class, documentId);
+		List<DocumentData> datas = crudService.hql(DocumentData.class, "from DocumentData where id.documentId = ? order by id.domainId", documentId);
+		String dataJson = new Gson().toJson(datas);
+		String hash = Sha512DigestUtils.shaHex(dataJson);
+		return hash;
+	}
+
+	@Override
+	public ProofDocument verify(String hash, String id) {
+		ProofDocument document = crudService.uniqueResultHql(ProofDocument.class, "from ProofDocument where chainHash = ?1", hash);
+		if (id.equals(document.getPersonalUser())) {
+			return document;
+		}
+		return null;
+	}
+
+}
