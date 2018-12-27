2fc0f343e84e312cb32624647db1ee2246565571
liheng <1172483433@qq.com>
2018-12-27 15:26:17 +0800

    2018-12-24 pdf优化

--- ftk-server/src/main/java/com/risepu/ftk/server/serviceImpl/ChainServiceImpl.java
+++ ftk-server/src/main/java/com/risepu/ftk/server/serviceImpl/ChainServiceImpl.java
@@ -23,31 +23,31 @@ import net.lc4ever.framework.service.GenericCrudService;
 @Service
 public class ChainServiceImpl implements ChainService {
 
-	private GenericCrudService crudService;
+    private GenericCrudService crudService;
 
-	@Autowired
-	public void setCrudService(GenericCrudService crudService) {
-		this.crudService = crudService;
-	}
+    @Autowired
+    public void setCrudService(GenericCrudService crudService) {
+        this.crudService = crudService;
+    }
 
-	@Override
-	public String sign(Long documentId) {
-		ProofDocument document = crudService.get(ProofDocument.class, documentId);
-		Template template = crudService.get(Template.class, document.getTemplate());
-		List<DocumentData> datas = crudService.hql(DocumentData.class, "from DocumentData where id.documentId = ? order by id.domainId", documentId);
-		String dataJson = new Gson().toJson(datas) + "##";
-		dataJson += new Gson().toJson(template);
-		String hash = Sha512DigestUtils.shaHex(dataJson + "##{documentId=" + documentId + ", organization=\"" + document.getOrganization() + "\", personal=\"" + document.getPersonalUser() + "\"}");
-		return hash.length() > 40 ? hash.substring(0, 40) : hash;
-	}
+    @Override
+    public String sign(Long documentId) {
+        ProofDocument document = crudService.get(ProofDocument.class, documentId);
+        Template template = crudService.get(Template.class, document.getTemplate());
+        List<DocumentData> datas = crudService.hql(DocumentData.class, "from DocumentData where id.documentId = ?1 order by id.domainId", documentId);
+        String dataJson = new Gson().toJson(datas) + "##";
+        dataJson += new Gson().toJson(template);
+        String hash = Sha512DigestUtils.shaHex(dataJson + "##{documentId=" + documentId + ", organization=\"" + document.getOrganization() + "\", personal=\"" + document.getPersonalUser() + "\"}");
+        return hash.length() > 40 ? hash.substring(0, 40) : hash;
+    }
 
-	@Override
-	public ProofDocument verify(String hash, String id) {
-		ProofDocument document = crudService.uniqueResultHql(ProofDocument.class, "from ProofDocument where chainHash = ?1", hash);
-		if (id.equals(document.getPersonalUser())) {
-			return document;
-		}
-		return null;
-	}
+    @Override
+    public ProofDocument verify(String hash, String id) {
+        ProofDocument document = crudService.uniqueResultHql(ProofDocument.class, "from ProofDocument where chainHash = ?1", hash);
+        if (id.equals(document.getPersonalUser())) {
+            return document;
+        }
+        return null;
+    }
 
 }
