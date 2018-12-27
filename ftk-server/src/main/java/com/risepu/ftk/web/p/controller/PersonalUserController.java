3b6d709ab7425f2585e99bca1b8bd13a16777e2c
liheng <1172483433@qq.com>
2018-12-27 12:48:20 +0800

    2018-12-24 pdf优化

--- ftk-server/src/main/java/com/risepu/ftk/web/p/controller/PersonalUserController.java
+++ ftk-server/src/main/java/com/risepu/ftk/web/p/controller/PersonalUserController.java
@@ -2,7 +2,6 @@ package com.risepu.ftk.web.p.controller;
 
 import java.util.Map;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import com.risepu.ftk.server.domain.ProofDocument;
@@ -10,7 +9,6 @@ import com.risepu.ftk.server.service.ChainService;
 import com.risepu.ftk.server.service.ProofDocumentService;
 import com.risepu.ftk.web.b.dto.PageRequest;
 import com.risepu.ftk.web.exception.NotLoginException;
-import org.apache.commons.fileupload.servlet.ServletRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,156 +29,156 @@ import com.risepu.ftk.web.p.dto.LoginResult;
 
 @RestController
 
-public class PersonalUserController implements PersonzalUserApi  {
+public class PersonalUserController implements PersonzalUserApi {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Autowired
-	private PersonalUserService personalService;
-	
-	@Autowired
-	private SmsService smsService;
+    @Autowired
+    private PersonalUserService personalService;
 
-	@Autowired
-	private ProofDocumentService proofDocumentService;
+    @Autowired
+    private SmsService smsService;
 
-	@Autowired
-	private ChainService chainService;
+    @Autowired
+    private ProofDocumentService proofDocumentService;
 
-
-	@Override
-	public ResponseEntity<Response<String>> personalScanDoc(String hash, HttpServletRequest request) {
-		request.getSession().setAttribute(Constant.getSessionChainHash(),hash);
-		return ResponseEntity.ok(Response.succeed("扫描单据成功"));
-	}
-
-	/**
-	 * 用户登录　通过扫描二维码跳转登录页面
-	 * @param loginRequest 
-	 * @param request
-	 * @return 需要
-	 */
-	@Override
-	public ResponseEntity<Response<LoginResult>> personalLogin(LoginRequest loginRequest,HttpServletRequest request){
-		
-		String smsCode =getSmsCode(request);
-		boolean identify = smsService.identify(loginRequest.getInCode(), smsCode);
-		LoginResult loginResult = new LoginResult();
-		
-		if(identify) {
-			String no = loginRequest.getCardNo();
-			/** 解析单据信息 */
-			String chainHash = (String) request.getSession().getAttribute(Constant.getSessionChainHash());
-			/** 校验用户输入的身份证号是否和单据信息一致 */
-			ProofDocument document = chainService.verify(chainHash, no);
-
-			if(document!=null) {
-				PersonalUser personalUser = personalService.findUserByNo(no);
-				if(personalUser!=null) {
-					loginResult.setMessage("登录成功");
-					loginResult.setPersonalUser(personalUser);
-				}else {
-					PersonalUser user = new PersonalUser();
-					user.setId(no);
-					user.setMobile(loginRequest.getPhone());
-					personalService.savePersonUser(user);
-					personalUser= user;
-				}
-				request.getSession().setAttribute(Constant.getSessionCurrUser(), personalUser);
-				
-				/** 根据身份证 查询新的请求授权的企业名和 当前授权流水的id*/
-				Map<String, Object> map  = personalService.findNewRequestByCardNo(no);
-				
-				if(map!=null) {
-					loginResult.setOrgName((String)map.get("orgName"));
-					loginResult.setStreamId((Long)map.get("streamId"));;
-				}
-				logger.debug("用户手机号--{}，登录成功",personalUser.getMobile());
-				return ResponseEntity.ok(Response.succeed(loginResult));
-			}else {
-				loginResult.setMessage("输入的身份证号和单据不匹配");
-				return ResponseEntity.ok(Response.failed(10, loginResult.getMessage()));
-			}
-		}else {
-			loginResult.setMessage("验证码输入错误");
-			return ResponseEntity.ok(Response.failed(2,loginResult.getMessage()));
-		}
-		
-	}
-
-	private String getSmsCode(HttpServletRequest request) {
-		return (String) request.getSession().getAttribute(Constant.getSessionVerificationCodeSms());
-	}
+    @Autowired
+    private ChainService chainService;
 
 
-	/**
-	 * 用户发起授权或拒绝
-	 * @param streamId 当前扫描流水id
-	 * @param state 授权状态
-	 * @param request
-	 * @return
-	 */
-	@Override
-	public ResponseEntity<Response<String>> personAuth( String streamId, String state, HttpServletRequest request) {
+    @Override
+    public ResponseEntity<Response<String>> personalScanDoc(String hash, HttpServletRequest request) {
+        request.getSession().setAttribute(Constant.getSessionChainHash(), hash);
+        return ResponseEntity.ok(Response.succeed("扫描单据成功"));
+    }
 
-		
-		String message ="";
-		PersonalUser personalUser = getCurrUser(request);
-		if(personalUser==null){
-			throw new NotLoginException();
-		}
-		AuthorizationStream stream =   personalService.findAuthorizationStreamById(Long.parseLong(streamId));
-		
-		if(stream==null) {
-			return ResponseEntity.ok(Response.failed(11,"错误的流水id"));
-		}
-		
-		/** 判断授权 */
-		if(Integer.parseInt(state)==(AuthorizationStream.AUTH_STATE_PASS)) {
-			/** 发送验证码 */
-			String code = smsService.sendCode(personalUser.getMobile());
-			stream.setAuthCode(code);
-			stream.setState(AuthorizationStream.AUTH_STATE_PASS);
-			message="授权码下发成功";
-			
-		}else {
-			stream.setState(AuthorizationStream.AUTH_STATE_REFUSE);
-			message="授权已拒绝";
-		}
-		
-		personalService.update(stream);
-		
-		return ResponseEntity.ok(Response.succeed(message));
-	}
+    /**
+     * 用户登录　通过扫描二维码跳转登录页面
+     *
+     * @param loginRequest
+     * @param request
+     * @return 需要
+     */
+    @Override
+    public ResponseEntity<Response<LoginResult>> personalLogin(LoginRequest loginRequest, HttpServletRequest request) {
+
+        String smsCode = getSmsCode(request);
+        boolean identify = smsService.identify(loginRequest.getInCode(), smsCode);
+        LoginResult loginResult = new LoginResult();
+
+        if (identify) {
+            String no = loginRequest.getCardNo();
+            /** 解析单据信息 */
+            String chainHash = (String) request.getSession().getAttribute(Constant.getSessionChainHash());
+            /** 校验用户输入的身份证号是否和单据信息一致 */
+            ProofDocument document = chainService.verify(chainHash, no);
+
+            if (document != null) {
+                PersonalUser personalUser = personalService.findUserByNo(no);
+                if (personalUser != null) {
+                    loginResult.setMessage("登录成功");
+                    loginResult.setPersonalUser(personalUser);
+                } else {
+                    PersonalUser user = new PersonalUser();
+                    user.setId(no);
+                    user.setMobile(loginRequest.getPhone());
+                    personalService.savePersonUser(user);
+                    personalUser = user;
+                }
+                request.getSession().setAttribute(Constant.getSessionCurrUser(), personalUser);
+
+                /** 根据身份证 查询新的请求授权的企业名和 当前授权流水的id*/
+                Map<String, Object> map = personalService.findNewRequestByCardNo(no);
+
+                if (map != null) {
+                    loginResult.setOrgName((String) map.get("orgName"));
+                    loginResult.setStreamId((Long) map.get("streamId"));
+                    ;
+                }
+                logger.debug("用户手机号--{}，登录成功", personalUser.getMobile());
+                return ResponseEntity.ok(Response.succeed(loginResult));
+            } else {
+                loginResult.setMessage("输入的身份证号和单据不匹配");
+                return ResponseEntity.ok(Response.failed(10, loginResult.getMessage()));
+            }
+        } else {
+            loginResult.setMessage("验证码输入错误");
+            return ResponseEntity.ok(Response.failed(2, loginResult.getMessage()));
+        }
+
+    }
+
+    private String getSmsCode(HttpServletRequest request) {
+        return (String) request.getSession().getAttribute(Constant.getSessionVerificationCodeSms());
+    }
 
 
+    /**
+     * 用户发起授权或拒绝
+     *
+     * @param streamId 当前扫描流水id
+     * @param state    授权状态
+     * @param request
+     * @return
+     */
+    @Override
+    public ResponseEntity<Response<String>> personAuth(String streamId, String state, HttpServletRequest request) {
 
 
-	/**
-	 * 查询历史授权记录
-	 * @param pageRequest 分页参数
-	 * @param request
-	 * @return
-	 */
-	@Override
-	public ResponseEntity<Response<PageResult<AuthHistoryInfo>>> getAuthInfoList(PageRequest pageRequest,
-																				 HttpServletRequest request){
-		PersonalUser user = getCurrUser(request);
-		if(user==null){
-			throw new NotLoginException();
-		}
-		
-		PageResult<AuthHistoryInfo> pageResult =  personalService.queryHistoryByParam(pageRequest.getKey(),pageRequest.getPageNo(),pageRequest.getPageSize(),user.getId());
-		
-		return ResponseEntity.ok(Response.succeed(pageResult));
-		
-	}
+        String message = "";
+        PersonalUser personalUser = getCurrUser(request);
+        if (personalUser == null) {
+            throw new NotLoginException();
+        }
+        AuthorizationStream stream = personalService.findAuthorizationStreamById(Long.parseLong(streamId));
+
+        if (stream == null) {
+            return ResponseEntity.ok(Response.failed(11, "错误的流水id"));
+        }
+
+        /** 判断授权 */
+        if (Integer.parseInt(state) == (AuthorizationStream.AUTH_STATE_PASS)) {
+            /** 发送验证码 */
+            String code = smsService.sendCode(personalUser.getMobile());
+            stream.setAuthCode(code);
+            stream.setState(AuthorizationStream.AUTH_STATE_PASS);
+            message = "授权码下发成功";
+
+        } else {
+            stream.setState(AuthorizationStream.AUTH_STATE_REFUSE);
+            message = "授权已拒绝";
+        }
+
+        personalService.update(stream);
+
+        return ResponseEntity.ok(Response.succeed(message));
+    }
 
 
+    /**
+     * 查询历史授权记录
+     *
+     * @param pageRequest 分页参数
+     * @param request
+     * @return
+     */
+    @Override
+    public ResponseEntity<Response<PageResult<AuthHistoryInfo>>> getAuthInfoList(PageRequest pageRequest,
+                                                                                 HttpServletRequest request) {
+        PersonalUser user = getCurrUser(request);
+        if (user == null) {
+            throw new NotLoginException();
+        }
+
+        PageResult<AuthHistoryInfo> pageResult = personalService.queryHistoryByParam(pageRequest.getKey(), pageRequest.getPageNo(), pageRequest.getPageSize(), user.getId());
+
+        return ResponseEntity.ok(Response.succeed(pageResult));
+
+    }
 
 
-	private PersonalUser getCurrUser(HttpServletRequest request) {
-		return (PersonalUser) request.getSession().getAttribute(Constant.getSessionCurrUser());
-	}
+    private PersonalUser getCurrUser(HttpServletRequest request) {
+        return (PersonalUser) request.getSession().getAttribute(Constant.getSessionCurrUser());
+    }
 
 }
