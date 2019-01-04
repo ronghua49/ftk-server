63cc02ab1e09958455070f8de2cd2005f4732bb9
liheng <1172483433@qq.com>
2019-01-04 16:02:48 +0800

    2018-12-29

--- ftk-server/src/main/java/com/risepu/ftk/web/m/controller/ReportController.java
+++ ftk-server/src/main/java/com/risepu/ftk/web/m/controller/ReportController.java
@@ -20,9 +20,9 @@ public class ReportController implements ReportApi {
     private GenericCrudService crudService;
 
     @Override
-    public ResponseEntity<Response<List>> getCount(String year) {
+    public ResponseEntity<Response<List>> getCount(Integer year) {
         List list = crudService.hql(DocumentNumber.class, "SELECT COUNT(1) AS number,MONTH(createTimestamp) as time FROM ProofDocument where number is not null and year(createTimestamp) = ?1 GROUP BY MONTH(createTimestamp) ORDER BY MONTH(createTimestamp)", year);
-        Integer total = (Integer) crudService.uniqueResultHql("SELECT COUNT(1) FROM ProofDocument where number is not null and year(createTimestamp) = ?1", year);
+        Long total = (Long) crudService.uniqueResultHql("SELECT COUNT(1) FROM ProofDocument where number is not null and year(createTimestamp) = ?1", year);
         list.add(total);
         return ResponseEntity.ok(Response.succeed(list));
     }
